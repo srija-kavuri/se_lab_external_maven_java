e98d389f4e28a328f7c21a11587a9ef4310cdd5d
srijakavuri <srijakavuri27@gmail.com>
2025-01-15 20:28:40 +0530

    java added

---
+++ src/main/java/com/app/demo/maven_java_ex/App.java
@@ -0,0 +1,10 @@
+package com.app.demo.maven_java_ex;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
